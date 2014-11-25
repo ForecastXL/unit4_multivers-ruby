de87e7e677cf503876ab3759f892f42046f8044d
Laurens Nutma <laurensnutma@gmail.com>
2014-11-25 14:46:46 +0100

    Add DepartmentNVL method

---
+++ lib/unit_4_multivers/department.rb
@@ -0,0 +1,16 @@
+module Unit4Multivers
+  class Client
+    def department_nvl(opts = {})
+      required = [:database]
+      check_required_parameters(required, opts)
+
+      get "/#{opts.fetch(:database)}/DepartmentNVL}", opts
+    end
+  end
+end
+
+
+
+
+
+
